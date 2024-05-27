a4ccaedfc151a1d7345f062b7caa194e48911a8e
5hymon <245843@edu.p.lodz.pl>
2024-05-27 10:34:42 +0200

    15. Testy przechodzą poprawnie

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -12,6 +12,7 @@ class RhymersDemo {
 
     }
 
+    //Testy poprawne
     private static void testRhymers(Rhymersfactory factory) {
         DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
                 factory.getFIFORhymer(), factory.getHanoiRhymer()};
